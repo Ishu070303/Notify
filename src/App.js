55eb9b9c99a9629e6adc05e11a3c9a92cd8d0481
Eshika Rawat <eshikarawat02@gmail.com>
2022-04-29 10:09:55 +0530

    updating login system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
+
 import './App.css';
+import Card from './components/card/Card';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
+  
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
   return (
    <div className='container'>
+     {user ? ( 
+       <>
+         <Navbar />
+         <Card />
+       </>
+     ) : (
      <div className='login'>
-       <input type='text' placeholder='Username' />
-         <button>LOGIN</button>
+       <input type='text' placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
+         <button onClick={() => setUser(username)}>LOGIN</button>
      </div>
+     )}
    </div>
   );
 }
