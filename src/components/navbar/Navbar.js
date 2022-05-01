e6fe5e636523433c05463b4a12925104820ea4fa
Eshika Rawat <eshikarawat02@gmail.com>
2022-05-01 20:38:46 +0530

    adding images and donw with navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+import Notification from '../img/notification.png';
+import Message from '../img/message.png';
+import Settings from '../img/settings.png';
+
+import './navbar.css';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        Navbar
+        <span className='logo'> Noti App </span>
+        <div className='icons'>
+            <div className='icon'>
+                <img src={Notification} className='iconImg' alt='' />
+                <div className='counter'>2</div>
+            </div>
+            <div className='icon'>
+                <img src={Message} className='iconImg' alt='' />
+                <div className='counter'>2</div>
+            </div>
+            <div className='icon'>
+                <img src={Settings} className='iconImg' alt='' />
+                <div className='counter'>2</div>
+            </div>
+        </div>
     </div>
   )
 }
